563293df5b9c930f93b0dfc999fcfc333b92453b
mopano <p.r.7.norouzi@gmail.com>
2023-07-12 07:57:53 -0700

    settingUp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Main from "./components/Main/Main";
+
+function App() {
+    return (
+        <react.Fragment>
+            <Main />
+        </react.Fragment>
+    );
+}
+
+export default App;
