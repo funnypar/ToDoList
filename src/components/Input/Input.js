0dae5e6d3ec286674001330d9c5d76f2ffdc8a53
mopano <p.r.7.norouzi@gmail.com>
2023-07-13 08:56:46 -0700

    GetDataFromUser

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,14 +1,23 @@
+import { useRef } from "react";
 import styles from "./Input.module.css";
 import Card from "../UI/Card/Card";
 
 const Input = () => {
+    const userEnter = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log(userEnter.current.value);
+    };
+
     return (
         <Card className={styles.card}>
-            <form className={styles.container}>
+            <form className={styles.container} onSubmit={submitHandler}>
                 <input
                     type="text"
                     placeholder="Add New Task"
                     className={styles.userInput}
+                    ref={userEnter}
                 />
                 <button className={styles.btn}>Add Task</button>
             </form>
